1c27be88e09249c99811e79a5a07154c1b7a581a
kawaxi <kawaxi@live.com>
2017-09-30 22:56:15 -0500

    Code optimizations

---
+++ lib/subterfurge-setup.py
@@ -0,0 +1,52 @@
+###SUBTERFUGE Setup Script Current as of version 1.1
+#Purpose of this script is to install packages required by Subterfuge
+#Error messages pertain to packages that may have failed to install properly.
+#Final steps configure Subterfuge to run on the local system
+#Setup script is configured to run on Kali Linux (tested on version 2.0) but should work on all debian-based systems
+
+import os
+
+#Dependencies
+
+#Scapy
+try:
+   os.system("apt-get -y -qq install python-scapy")
+except:
+   print "[!] Critical package failed to install: python-scapy"
+
+#Python-Twisted
+try:
+   os.system("apt-get -y -qq install python-twisted")
+except:
+   print "[!] Important package failed to install: python-twisted"
+   print "[-] python-twisted is required to perform SSL Stripping attacks"
+
+#Python-Django
+try:
+   os.system("apt-get -y -qq install python-django")
+except:
+   print "[!] Critical package failed to install: python-django"
+
+#Arptables
+try:
+   os.system("apt-get  -y -qq install arptables")
+except:
+   print "[!] Critical package failed to install: arptables"
+
+#DHCPD
+try:
+   os.system("apt-get -y -qq install dhcpd")
+except:
+   print "[!] Package failed to install: dhcpd"
+
+
+
+
+#Install Subterfuge
+try:
+   os.system("cp -R ../subterfuge /usr/share/subterfuge")
+   os.system("cp xsubterfuge /bin/subterfuge")
+   os.system("chmod +x /bin/subterfuge")
+   os.system("cp manage.py /usr/share/")
+except:
+   print "[!] Critical Error! Subterfuge configuration failed"
